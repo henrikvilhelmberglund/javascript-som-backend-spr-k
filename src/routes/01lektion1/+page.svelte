274bdae47b70fa0c95543526294265e07e4ed277
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-04-25 17:18:06 +0200

    feat: :sparkles: add a page that gets the data from the load function and displays the data

--- src/routes/01lektion1/+page.svelte
+++ src/routes/01lektion1/+page.svelte
@@ -1,9 +1,15 @@
 <script>
-  
+	export let data;
+	console.log(data.books);
 </script>
 
-
+<main class="[&>*]:m-4">
+	{#each data.books as object}
+		{#each Object.entries(object) as [key, value]}
+			<p class="text-2xl">{key}:{value}</p>
+		{/each}
+	{/each}
+</main>
 
 <style>
-  
 </style>
