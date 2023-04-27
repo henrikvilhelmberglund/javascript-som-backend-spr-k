535e54c48a95f8b5f814bc07fc43a90205c2e45f
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-04-27 08:32:04 +0200

    feat: :sparkles: add Nav

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,38 @@
+<script>
+	export let routes;
+	export let prefix = "";
+	export let level;
+	import { page } from "$app/stores";
+	import { base } from "$app/paths";
+</script>
+
+<nav class="flex flex-col">
+	<div class="flex-row [&>*]:m-2">
+		{#if level === 1}
+			<a class="rounded-lg bg-purple-400 px-4 text-black hover:bg-purple-500" href="{base}/"
+				>Home</a>
+		{/if}
+		{#each routes as route}
+			{#if level === 1}
+				<a
+					class="rounded-lg bg-purple-400 px-4 text-black hover:bg-purple-500"
+					href="{base}{prefix}{route}">{route.replace("/", "")}</a>
+			{:else if level === 2}
+				<a
+					class="rounded-lg bg-violet-400 px-4 text-black hover:bg-violet-500"
+					href="{base}{prefix}{route}">{route.replace("/", "")}</a>
+			{:else if level === 3}
+				<a
+					class="rounded-lg bg-indigo-400 px-4 text-black hover:bg-indigo-500"
+					href="{base}{prefix}{route}">{route.replace("/", "")}</a>
+			{:else if level === 4}
+				<a
+					class="rounded-lg bg-blue-400 px-4 text-black hover:bg-blue-500"
+					href="{base}{prefix}{route}">{route.replace("/", "")}</a>
+			{/if}
+		{/each}
+	</div>
+</nav>
+
+<style>
+</style>
