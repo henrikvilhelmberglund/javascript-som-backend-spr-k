49ff467e5dd014e0615eecb4e4e8b7481421b0ac
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-05-13 17:33:45 +0200

    fix: :bug: update links

--- src/routes/blog-api/+layout.svelte
+++ src/routes/blog-api/+layout.svelte
@@ -1,8 +1,8 @@
 <script>
 	import Nav from "$lib/Nav.svelte";
 
-	const routes = [{ link: "/", display: "Blog home" }];
-	const prefix = "/blog";
+	const routes = [{ link: "/", display: "Blog API version home" }];
+	const prefix = "/blog-api";
 </script>
 
 <Nav {routes} {prefix} level={2} />
