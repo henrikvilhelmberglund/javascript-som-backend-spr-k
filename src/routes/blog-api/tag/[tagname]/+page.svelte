49230329a327e696365d16fffa5839b93b994f9e
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-05-13 17:34:42 +0200

    feat: :sparkles: add slug in tag route

--- src/routes/blog-api/tag/[tagname]/+page.svelte
+++ src/routes/blog-api/tag/[tagname]/+page.svelte
@@ -2,13 +2,14 @@
 	import Footer from "$lib/Footer.svelte";
 	import Post from "../../PostAPI.svelte";
 	import { toNiceDate } from "$lib/helpers.js";
+	import { page } from "$app/stores";
 
 	export let data;
 </script>
 
 <main class="[&>*]:m-4">
-  <div class="flex items-center justify-center gap-12">
-    <h1 class="text-3xl">Blog</h1>
+  <div class="flex flex-col items-center justify-center gap-12">
+    <h1 class="text-3xl">Posts tagged with #{$page.params.tagname} </h1>
 		{#each data.posts as post}
 			<Post {post} />
 		{/each}
