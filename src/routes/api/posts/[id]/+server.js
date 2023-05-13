1d66cd972cca01e5a042691c6de85b665505ff02
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-05-13 17:17:45 +0200

    fix: :bug: fix API PUT function

--- src/routes/api/posts/[id]/+server.js
+++ src/routes/api/posts/[id]/+server.js
@@ -2,6 +2,9 @@ import { error, json } from "@sveltejs/kit";
 import { getPostsCollection } from "$lib/hooks.server";
 import { ObjectId } from "mongodb";
 import chalk from "chalk";
+
+const postsCollection = await getPostsCollection();
+
 export async function GET({ params }) {
 	const offsetNumber = +params.id - 1;
 	const result = await getPostsCollection
@@ -18,24 +21,34 @@ export async function GET({ params }) {
 	});
 }
 
-// export async function PUT({ request, params }) {
-// 	const body = await request.json();
-// 	const offsetNumber = +params.id - 1;
-// 	const post = await getPostsCollection
-// 		.find({})
-// 		.skip(offsetNumber)
-// 		.sort({ _id: 1 })
-// 		.limit(1)
-// 		.toArray();
+export async function PUT({ request, params }) {
+	const incomingBody = await request.json();
 
-// 	// const result = await postsCollection.findOne({ _id: new ObjectId(req.params.id) });
-// 	// const post = await postsCollection.findOne({ _id: new ObjectId(req.params.id) });
-// 	const result = await getPostsCollection.updateOne(post[0], { $set: body });
-// 	console.log(result);
-// 	return json({
-// 		result,
-// 	});
-// }
+	let { title, content, date, tags } = incomingBody;
+	tags = tags = tags.replaceAll(" ,", ",").split(",");
+	const body = { title, content, date, tags };
+
+	const postId = params.id;
+	const post = await postsCollection.findOne({ _id: new ObjectId(postId) });
+
+	// ? for blod /1 etc thingy, not BSON
+	// const offsetNumber = +params.id - 1;
+	// const post = await getPostsCollection
+	// 	.find({})
+	// 	.skip(offsetNumber)
+	// 	.sort({ _id: 1 })
+	// 	.limit(1)
+	// 	.toArray();
+	// const result = await getPostsCollection.updateOne(post[0], { $set: body });
+
+	// const result = await postsCollection.findOne({ _id: new ObjectId(req.params.id) });
+	// const post = await postsCollection.findOne({ _id: new ObjectId(req.params.id) });
+	const result = await postsCollection.updateOne(post, { $set: body });
+	console.log(result);
+	return json({
+		result,
+	});
+}
 
 export async function DELETE({ params }) {
 	// app.delete("/api/posts/:id", async (req, res) => {
@@ -52,8 +65,6 @@ export async function DELETE({ params }) {
 	// console.table({ a:"Welcome to the app!"});
 	const postId = params.id;
 	console.log(chalk.blue(params.id));
-
-	const postsCollection = await getPostsCollection();
 
 	const post = await postsCollection.findOne({
 		_id: new ObjectId(postId),
