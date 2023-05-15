be754bc19240411610f1fcb36671732b85948c2d
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-05-15 11:16:55 +0200

    feat: :sparkles: start exercises

---
+++ authentication.js
@@ -0,0 +1,101 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import { MongoClient } from "mongodb";
+
+const app = express();
+const port = 3000;
+// tydligare vad man menar, 5 sek * 60 sekunder på minut * 1000 millisekunder
+const FIVE_MINUTES = 5 * 60 * 1000;
+
+const client = new MongoClient("mongodb://127.0.0.1:27017");
+// i produktionskod, lägg in i try
+await client.connect();
+
+const db = client.db("loginproject");
+const userCollection = db.collection("users");
+
+app.use(cookieParser());
+app.use(express.static("publiccookie"));
+app.use(express.json());
+app.use(
+	session({
+		resave: false,
+		saveUninitialized: false,
+		secret: "shhh very secret",
+	})
+);
+
+// ? 1.
+// app.post("/login", async (req, res) => {
+// 	console.log(req.body);
+// 	if (req.body.loginName === "admin" && req.body.loginPass === "12345") {
+// 		req.session.user = "admin";
+// 		res.json({ user: "admin" });
+// 	} else {
+// 		res.status(401).send("Unauthorized");
+// 	}
+// });
+
+// ? 2
+// app.post("/api/login", async (req, res) => {
+// 	console.log(req.body);
+// 	if (req.body.loginName === "admin" && req.body.loginPass === "12345") {
+// 		req.session.user = "admin";
+// 		res.json({ user: "admin" });
+// 	} else {
+// 		res.status(401).json({ error: "Unauthorized" });
+// 	}
+// });
+
+// ? 3
+app.post("/api/login", async (req, res) => {
+	console.log(req.body);
+	const user = await userCollection.findOne({
+		user: req.body.loginName,
+		pass: req.body.loginPass,
+	});
+	if (user) {
+		req.session.user = user;
+		res.json({
+			user: user.user,
+		});
+	} else {
+		res.status(401).json({ error: "Unauthorized" });
+	}
+});
+
+app.post("/api/register", async (req, res) => {
+	const user = await userCollection.insertOne({
+		user: req.body.registerName,
+		pass: req.body.registerPass,
+	});
+	res.json({
+		user: user.user,
+	});
+});
+
+app.get("/api/user", (req, res) => {
+	console.log(req.session.user);
+	res.json({ user: req.session.user });
+});
+
+app.post("/api/logout", (req, res) => {
+	req.session.destroy(() => {
+		res.json({ loggedIn: false });
+	});
+});
+
+app.get("/api/cookie", (req, res) => {
+	console.log(req.cookies);
+	console.log(req.session);
+	res.send("thanks!");
+});
+
+app.listen(port);
+
+const createCookie = () => {
+	document.cookie = "name=Henrik";
+	document.cookie = "profession=Frontend Developer";
+	console.log(document.cookie);
+};
--- publiccookie/main.js
+++ publiccookie/main.js
@@ -1,11 +1,20 @@
+const loginDiv = document.querySelector("#login-div");
 const loginForm = document.querySelector("#login");
+const logoutForm = document.querySelector("#logout");
 const loginName = document.querySelector("#username");
 const loginPass = document.querySelector("#password");
+
+const registerDiv = document.querySelector("#register-div");
+const registerForm = document.querySelector("#register");
+const registerName = document.querySelector("#register-username");
+const registerPass = document.querySelector("#register-password");
+
+let myUser;
 
 loginForm.addEventListener("submit", async (e) => {
 	e.preventDefault();
 
-	const res = await fetch("/login", {
+	const res = await fetch("/api/login", {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json",
@@ -16,13 +25,70 @@ loginForm.addEventListener("submit", async (e) => {
 		}),
 	});
 	const data = await res.json();
+	myUser = await getUser();
 	console.log("login result", data);
 });
 
+registerForm.addEventListener("submit", async (e) => {
+	e.preventDefault();
+
+	const res = await fetch("/api/register", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			registerName: registerName.value,
+			registerPass: registerPass.value,
+		}),
+	});
+	const data = await res.json();
+
+	loginDiv.style = "display: block;";
+	registerDiv.style = "display: none;";
+	logoutForm.style = "display: none;";
+	// loginDiv.innerHTML = "";
+	const h1 = document.createElement("h1");
+	h1.innerHTML = `Thank you for registering!!`;
+	document.body.append(h1);
+
+	console.log("register result", data);
+});
+
+logoutForm.addEventListener("submit", async (e) => {
+	e.preventDefault();
+
+	const res = await fetch("/api/logout", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			logout: true,
+		}),
+	});
+	const data = await res.json();
+	myUser = await getUser();
+	console.log("logout result", data);
+	location.reload("/");
+});
+
 const getUser = async () => {
-	const res = await fetch("/user");
+	const res = await fetch("/api/user");
 	const data = await res.json();
 	console.log("user:", data);
+	if (data.user !== undefined) {
+		loginDiv.style = "display: none;";
+		registerDiv.style = "display: none;";
+		// loginDiv.innerHTML = "";
+		const h1 = document.createElement("h1");
+		h1.innerHTML = `Welcome ${data.user.user}!`;
+		document.body.append(h1);
+		logoutForm.style = "display: block;";
+	} else {
+		logoutForm.style = "display: none;";
+	}
+	return data;
 };
 
 getUser();
